acff703346ca15540f9819c24a89860685fb834a
Bastien Krettly <bastien.krettly@wildcodeschool.com>
2021-11-03 14:43:02 +0100

    fix password encryption

--- src/Model/UserModel.php
+++ src/Model/UserModel.php
@@ -4,14 +4,20 @@ namespace App\Model;
 
 class UserModel
 {
-    const TEST_USER = [
-        'email' => 'tacos@mail.com',
-        'password' => 'tacostacos',
-        'avatar' => 'https://randomuser.me/api/portraits/men/70.jpg',
-        'phone' => '(911)-579-9788'
-    ];
+    private array $testUser;
+
+    public function __construct()
+    {
+        $this->testUser = [
+            'email' => 'tacos@mail.com',
+            'password' => password_hash('tacostacos', PASSWORD_BCRYPT),
+            'avatar' => 'https://randomuser.me/api/portraits/men/70.jpg',
+            'phone' => '(911)-579-9788'
+        ];
+    }
 
     // TODO: create a sign in fonction and store the user infos in session
+    // use password_verify: https://www.php.net/manual/en/function.password-verify.php
 
     // TODO: create a sign out fonction and clear the session
 }
