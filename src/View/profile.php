7e2a9e274a738be095f5d2265723a5cd2f6512ce
Bastien Krettly <bastien_krettly@wilder.school>
2021-11-03 11:53:12 +0100

    Initial commit

---
+++ src/View/profile.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Best Website Ever - Profile</title>
+    <link rel="stylesheet" href="/assets/css/style.css">
+</head>
+<body>
+    <?php require_once __DIR__ . '/_navigation.php'; ?>
+    <!-- TODO: if the user isn't logged, redirect to /login -->
+
+    <!-- TODO: if the user is logged, show their profile -->
+
+</body>
+</html>
